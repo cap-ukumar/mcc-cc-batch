bde8e09be6acdc1c5cdb59ca8faed5dbb377e03f
amrajag <amrajag@DESKTOP-4FQV3ML.cap.org>
2022-12-26 19:37:43 +0530

    Queries added

---
+++ src/main/java/org/cap/cc/batch/model/PrintDetailEntity.java
@@ -0,0 +1,24 @@
+package org.cap.cc.batch.model;
+
+public class PrintDetailEntity {
+	
+	private String printdetailId;
+	private String packettypeId;
+
+	public String getPrintdetailId() {
+		return printdetailId;
+	}
+
+	public void setPrintdetailId(String printdetailId) {
+		this.printdetailId = printdetailId;
+	}
+	
+	public String getPackettypeId() {
+		return packettypeId;
+	}
+
+	public void setPackettypeId(String packettypeId) {
+		this.packettypeId = packettypeId;
+	}
+
+}
