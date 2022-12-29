bc32917cabbe695dcb6570737e0ea62cd91e1e9a
Uday Kumar (ns) <ukumar@cap.org>
2022-12-29 14:06:34 +0530

    queries

--- src/main/java/org/cap/cc/batch/utils/CommonUtils.java
+++ src/main/java/org/cap/cc/batch/utils/CommonUtils.java
@@ -3,11 +3,14 @@ package org.cap.cc.batch.utils;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
+import java.util.UUID;
 
 public class CommonUtils {
 	
 	
 	public static Properties configs ;
+	
+	private static String s_UUID = null;
 	
 	
 	public static void loadProperties(InputStream inputStream) {
@@ -22,6 +25,18 @@ public class CommonUtils {
 	public static String getProperty(String key) {
 		return configs.getProperty(key);
 	}
+	
+	//You can use special_instr_t CHAR(255) in ptt_task table for checklist batch.
+	public static String getUUID()
+	{
+		if(s_UUID == null) {
+			s_UUID = UUID.randomUUID().toString();
+		}
+		
+		return s_UUID;
+	}
+	
+	
 	
 
 }
